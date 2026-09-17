d24e08c15ad3a99e40fc2692efc349cede1b61b9
María Cecilia <lunama.cecilia@live.com>
2026-09-17 18:52:09 -0300

    logica de registro local

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
 console.log("Hola mundo");
+
+const nombreInput = document.getElementById("nombre");
+const emailInput = document.getElementById("email");
+const edadInput= document.getElementById("edad");
+const contraseniaInput = document.getElementById("contrasenia");
+const confirmarContraseniaInput =  document.getElementById("confirmar-contrasenia");
+
+const registrarUsuario = (event) => {
+    event.preventDefault()
+
+    const nombre = nombreInput.value;
+    const email = emailInput.value;
+    const edad = edadInput.value;
+    const contrasenia = contraseniaInput.value;
+    const confirmarContrasenia = confirmarContraseniaInput.value;
+
+    const edadAceptable = 18;
+
+    if (contrasenia !== confirmarContrasenia){
+        alert("Las contraseñas no coinciden");
+        console.error(contrasenia, confirmarContrasenia, "las contraseñas no coinciden");
+        return;
+    }
+
+    if(edad < edadAceptable){
+        alert("Debes ser mayor de edad");
+        console.error(edad,"La edad no es válida para registrarse");
+        return;
+    }
+    localStorage.setItem("emailUsuario", email);
+    window.location.href = "bienvenida.html";
+}
+
+const validarRegistro = () => {
+    const userRegistrado = localStorage.getItem("emailUsuario");
+
+    if (userRegistrado) {
+        window.location="bienvenida.html"
+    }
+}
+
+if (window.location.pathname !== "/bienvenida.html") {
+    validarRegistro()
+}
+
+const logout = () => {
+    localStorage.clear();
+    window.location.href="index.html";
+}
